7b2597c1056ee0392d61d78d2e7d5095eb14facb
nermeen16ali <nermeenali162000@gmail.com>
2024-06-08 03:26:39 +0300

    d

--- js/logo.js
+++ js/logo.js
@@ -1,22 +1,3 @@
-// var fileInputs = document.querySelectorAll('.upload-product-img');
-// const companyLogoParent = document.querySelector(".companyLogo");
-
-// fileInputs.forEach(function(input){
-//     input.addEventListener("change", function() {
-//         const file = this.files[0];
-//         const reader = new FileReader();
-      
-//         reader.onload = function(e) {
-//             var img = input.previousElementSibling.querySelector('img');
-//             img.src = e.target.result;
-//             img.classList.remove('d-none');
-//             img.classList.add('img-fluid');
-//             input.innerHTML = '';
-//         };
-      
-//         reader.readAsDataURL(file);
-//     });
-// })
 var fileInputs = document.querySelectorAll('.upload-product-img');
 const companyLogoParent = document.querySelector(".companyLogo");
 
